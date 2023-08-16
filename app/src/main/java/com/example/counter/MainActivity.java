e80d8d86c949e24eaa7b361a90168bf2a1a72b38
Shoba-755 <tappetashoba@gmail.com>
2023-08-16 21:48:26 +0530

    Counter App

---
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.counter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text1,text2;
+    Button button;
+    int counter=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text1 = findViewById(R.id.textView);
+        text2 = findViewById(R.id.textView2);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text2.setText(" "+ Counter());
+            }
+        });
+    }
+    private int Counter(){
+       return ++counter;
+    }
+}
